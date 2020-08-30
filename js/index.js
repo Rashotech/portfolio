00617c4070fc7183f30781a95255c5a461c04c9e
Rashotech <rashotech@gmail.com>
2020-08-30 10:03:03 +0100

    Masonry Grid Moment Section Added

--- js/index.js
+++ js/index.js
@@ -13,9 +13,45 @@ hamburger.addEventListener('click', function () {
   }
 });
 
+// Navbar collapse when a link is clicked
 window.addEventListener('click', function (e) {
   const navCollapse = document.querySelector('.navbar-collapse');
   if(e.target && e.target.className == "nav-link") {
     navCollapse.style.height = `0`;
   }
+});
+
+// Moment Pics zoom in
+
+var gallery = document.querySelector('#gallery');
+var getVal = function (elem, style) { return parseInt(window.getComputedStyle(elem).getPropertyValue(style)); };
+var getHeight = function (item) { return item.querySelector('.content').getBoundingClientRect().height; };
+var resizeAll = function () {
+    var altura = getVal(gallery, 'grid-auto-rows');
+    var gap = getVal(gallery, 'grid-row-gap');
+    gallery.querySelectorAll('.gallery-item').forEach(function (item) {
+        var el = item;
+        el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
+    });
+};
+gallery.querySelectorAll('img').forEach(function (item) {
+    item.classList.add('byebye');
+    if (item.complete) {
+        console.log(item.src);
+    }
+    else {
+        item.addEventListener('load', function () {
+            var altura = getVal(gallery, 'grid-auto-rows');
+            var gap = getVal(gallery, 'grid-row-gap');
+            var gitem = item.parentElement.parentElement;
+            gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+            item.classList.remove('byebye');
+        });
+    }
+});
+window.addEventListener('resize', resizeAll);
+gallery.querySelectorAll('.gallery-item').forEach(function (item) {
+    item.addEventListener('click', function () {        
+        item.classList.toggle('full');        
+    });
 });
